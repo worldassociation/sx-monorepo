721f7069b709f89f10b4f380a764cafe33fb36df
blaisekonya <blaisekonya@gmail.com>
2024-10-19 07:45:24 +0200

    Update wording again

--- apps/ui/src/components/Landing/Flow.vue
+++ apps/ui/src/components/Landing/Flow.vue
@@ -37,7 +37,7 @@ const STEPS = [
     <UiContainer class="!max-w-screen-lg">
       <div class="pb-6 max-w-[600px] text-center mx-auto">
         <h1 class="mb-4 font-display !text-[36px] sm:!text-[44px]">
-          A democratic organization open for all
+          Start shaping the future of humanity
         </h1>
       </div>
     </UiContainer>
