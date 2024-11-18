b2ab8f18199d769e237df704431c256dcc9580e3
blaisekonya <blaisekonya@gmail.com>
2024-11-18 02:49:36 +0100

    Increase site messenger's z-index

--- apps/ui/src/components/Messenger.vue
+++ apps/ui/src/components/Messenger.vue
@@ -4,7 +4,7 @@ import { HELPDESK_URL } from '@/helpers/constants';
 
 <template>
   <a :href="HELPDESK_URL" target="_blank"
-    class="hidden md:block fixed bottom-4 right-4 rounded-full bg-skin-link text-skin-bg p-2">
+    class="hidden md:block fixed bottom-4 right-4 rounded-full bg-skin-link text-skin-bg p-2 z-50">
     <IS-chat class="inline-block size-[32px]" />
   </a>
 </template>
