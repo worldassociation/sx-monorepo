5d8be8f5d0eee528a21b26937aa994bade1f2f64
blaisekonya <blaisekonya@gmail.com>
2024-11-18 02:24:17 +0100

    Fix WA link color issue

--- apps/ui/src/components/App/BottomNav.vue
+++ apps/ui/src/components/App/BottomNav.vue
@@ -21,9 +21,15 @@ const menu = [
   {
     link: { path: '/s:worldassociation.eth' },
     icon: defineComponent({
+      props: {
+        isActive: Boolean
+      },
       render() {
         return h('div', {
-          class: 'box-border h-[18px] w-[18px] rounded-full border-[3px] border-skin-primary'
+          class: [
+            'box-border h-[18px] w-[18px] rounded-full border-[3px]',
+            this.isActive ? 'border-skin-link' : 'border-skin-text'
+          ]
         });
       }
     })
@@ -45,8 +51,10 @@ const menu = [
     <div class="flex px-4 justify-between">
       <AppLink v-for="(item, i) in menu" :key="i" :to="item.link"
         class="inline-flex flex-col text-center truncate justify-center max-w-[120px]"
-        :class="route.name === item.link.name ? 'text-skin-link' : 'text-skin-text'">
-        <component :is="item.icon" class="mx-auto size-4" />
+        :class="(item.link.name ? route.name === item.link.name : route.path === item.link.path) ? 'text-skin-link' : 'text-skin-text'">
+        <component :is="item.icon"
+          :is-active="item.link.name ? route.name === item.link.name : route.path === item.link.path"
+          class="mx-auto size-4" />
       </AppLink>
     </div>
   </nav>
